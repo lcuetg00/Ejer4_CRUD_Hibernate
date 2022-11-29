d6058b52dd1cb22299b0036569a4661c78d27632
lcuetg00 <47599827+lcuetg00@users.noreply.github.com>
2022-11-29 11:41:07 +0100

    Actualizadas funcionalidades de validar número de identificación

--- src/main/java/com/ejer/controller/ControladorCliente.java
+++ src/main/java/com/ejer/controller/ControladorCliente.java
@@ -1,4 +1,45 @@
 package com.ejer.controller;
 
+import com.ejer.service.ServiceCliente;
+
+import java.security.InvalidParameterException;
+
 public class ControladorCliente {
+
+    /**
+     * Patrón que siguen los NIE: números más una letra final
+     */
+    public static final String REGEX_NIE = "[0-9]++[A-Z]";
+    /**
+     * Patrón que siguen los NIF: Letra X o T seguido de número y una letra final
+     */
+    public static final String REGEX_NIF = "[XT][0-9]++[A-Z]";
+    /**
+     * Patrón que siguen los CIF: Letra seguida de números y finalmente puede acabar en una letra o en un número
+     */
+    public static final String REGEX_CIF = "[A-Z][0-9]++[A-Z_0-9]";
+
+    /**
+     * Comprueba si un número de identificación es válido
+     * @param numeroIdentificacion
+     * @return
+     */
+    public static boolean validarNieNifCif(final String numeroIdentificacion) {
+        if(numeroIdentificacion == null) {
+            throw new InvalidParameterException();
+        }
+        boolean esValido = false;
+
+        if(numeroIdentificacion.matches(REGEX_NIE)) {
+            esValido = ServiceCliente.validarNie(numeroIdentificacion);
+        }
+        if(numeroIdentificacion.matches(REGEX_NIF)) {
+            esValido = ServiceCliente.validarNif(numeroIdentificacion);
+        }
+        if(numeroIdentificacion.matches(REGEX_CIF)) {
+            esValido = ServiceCliente.valifarCif(numeroIdentificacion);
+        }
+
+        return esValido;
+    }
 }
