7e9eaa38ec2043fe61687dca71dea7c265b1c220
lcuetg00 <47599827+lcuetg00@users.noreply.github.com>
2022-12-02 09:10:24 +0100

    Actualizado ControladorCliente

--- src/main/java/com/ejer/controller/ControladorCliente.java
+++ src/main/java/com/ejer/controller/ControladorCliente.java
@@ -9,22 +9,22 @@ public class ControladorCliente {
     /**
      * Patrón que siguen los NIE: números más una letra final
      */
-    public static final String REGEX_NIE = "[0-9]++[A-Z]";
+    private static final String REGEX_NIE = "[0-9]++[A-Z]";
     /**
      * Patrón que siguen los NIF: Letra X o T seguido de número y una letra final
      */
-    public static final String REGEX_NIF = "[XT][0-9]++[A-Z]";
+    private static final String REGEX_NIF = "[XT][0-9]++[A-Z]";
     /**
      * Patrón que siguen los CIF: Letra seguida de números y finalmente puede acabar en una letra o en un número
      */
-    public static final String REGEX_CIF = "[A-Z][0-9]++[A-Z_0-9]";
+    private static final String REGEX_CIF = "[A-Z][0-9]++[A-Z_0-9]";
 
     /**
      * Comprueba si un número de identificación es válido
      * @param numeroIdentificacion
      * @return
      */
-    public static boolean validarNieNifCif(final String numeroIdentificacion) {
+    public static boolean validarNumeroDocumentacion(final String numeroIdentificacion) {
         if(numeroIdentificacion == null) {
             throw new InvalidParameterException();
         }
