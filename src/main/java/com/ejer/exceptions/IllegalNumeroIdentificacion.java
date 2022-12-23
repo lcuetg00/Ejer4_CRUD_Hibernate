5e8f6dcb78952d830f9d951c65cd5ef1b1a213e0
lcuetg00 <47599827+lcuetg00@users.noreply.github.com>
2022-12-23 09:16:29 +0100

    Delete src/main/java/com/ejer/exceptions directory

--- src/main/java/com/ejer/exceptions/IllegalNumeroIdentificacion.java
+++
@@ -1,7 +0,0 @@
-package com.ejer.exceptions;
-
-public class IllegalNumeroIdentificacion extends Exception {
-    public IllegalNumeroIdentificacion(String mensaje) {
-        super(mensaje);
-    }
-}
